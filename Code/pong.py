c8c33e64ddf4ae827f385036ec161526a0ec1798
Benjamin <Benjamin@vonmetzen.de>
2024-02-13 14:08:36 +0100

    next

---
+++ Code/pong.py
@@ -0,0 +1,161 @@
+import pygame
+from math import sin, cos, pi
+from time import sleep
+import numpy as np
+from typing import Tuple
+from enum import Enum
+import random
+import os
+
+os.environ["SDL_VIDEO_WINDOW_POS"] = "%d,%d" % (1500, 450)
+BILDSCHIRM_X = 320
+BILDSCHIRM_Y = 200
+
+
+SCHLAEGER_LAENGE = 25
+SCHLAEGER_GESCHWINDIGKEIT = 1
+BEWEGUNG_PUNKTE = -1
+TREFFER_PUNKTE = 10
+VERLOREN_PUNKTE = -100
+GEWONNEN_PUNKTE = 100
+
+
+class Game:
+    def __init__(self, mit_grafik: bool):
+        self.schlaeger_y = BILDSCHIRM_Y / 2
+        self.ball_x = 1
+        self.ball_y = 100
+        self.ball_geschwindigkeit = 5
+        self.ball_richtung = 45 / 180 * pi
+
+        self.grafik = False
+        self.surface = None
+        self.quit = False
+
+        self.treffer = 0
+        self.spiellaenge = 20
+        if mit_grafik:
+            pygame.init()
+            pygame.display.set_mode(size=(BILDSCHIRM_X + 1, BILDSCHIRM_Y))
+            self.grafik = mit_grafik
+            self.surface = pygame.display.get_surface()
+
+    def reset(self, winkel: int = 10) -> np.ndarray:
+        """Setzt die Spielumgebung zurück.
+
+
+        Args:
+            mit_grafik (bool): Flag zur initialisierung des pyGame environments
+
+        Returns:
+            np.ndarray: Vector des aktuellen Zustands
+        """
+
+        self.quit = False
+        self.treffer = 0
+        self.schlaeger_y = BILDSCHIRM_Y / 2
+        self.ball_x = 1
+        self.ball_y = 100
+        self.ball_geschwindigkeit = 5
+        self.ball_richtung = random.randint(-winkel, winkel) / 180 * pi
+
+        return np.array(
+            [
+                self.schlaeger_y,
+                self.ball_x,
+                self.ball_y,
+                self.ball_geschwindigkeit,
+                self.ball_richtung,
+            ]
+        )
+
+    def pong_step(
+        self, action: int, learning_mode: bool = True
+    ) -> Tuple[np.ndarray, float, bool]:
+        """Führe einen Spielschritt aus
+
+        Args:
+            action (int): 0: tu nichts, 1 hoch, 2 runter
+
+        Returns:
+            Tuple[np.ndarray, float, bool]: Zustand, Reward, Ende des Spiels
+        """
+        belohnung = 0
+        if action == 1:
+            if self.schlaeger_y >= 1:
+                self.schlaeger_y -= SCHLAEGER_GESCHWINDIGKEIT
+                belohnung += BEWEGUNG_PUNKTE
+        elif action == 2:
+            if self.schlaeger_y < BILDSCHIRM_Y - SCHLAEGER_LAENGE:
+                self.schlaeger_y += SCHLAEGER_GESCHWINDIGKEIT
+                belohnung += BEWEGUNG_PUNKTE
+        self.ball_x = self.ball_x + self.ball_geschwindigkeit * cos(self.ball_richtung)
+        self.ball_y = self.ball_y + self.ball_geschwindigkeit * sin(self.ball_richtung)
+
+        if self.ball_x <= 0:
+            self.ball_richtung = ((2 * pi - self.ball_richtung) + pi) % (2 * pi)
+            self.ball_x = -self.ball_x
+        if self.ball_y > BILDSCHIRM_Y:
+            self.ball_richtung = (2 * pi) - self.ball_richtung
+            self.ball_y = BILDSCHIRM_Y - (self.ball_y - BILDSCHIRM_Y)
+        if self.ball_y < 0:
+            self.ball_richtung = (2 * pi) - self.ball_richtung
+            self.ball_y = -self.ball_y
+        if self.ball_x >= BILDSCHIRM_X:
+            belohnung -= abs(self.schlaeger_y + SCHLAEGER_LAENGE / 2 - self.ball_y)
+            if (
+                self.ball_y >= self.schlaeger_y
+                and self.ball_y <= self.schlaeger_y + SCHLAEGER_LAENGE
+            ):
+                self.ball_richtung = ((2 * pi - self.ball_richtung) + pi) % (2 * pi)
+                self.ball_x = BILDSCHIRM_X - (self.ball_x % BILDSCHIRM_X)
+                belohnung += TREFFER_PUNKTE
+                self.treffer += 1
+                if self.treffer >= self.spiellaenge:
+                    belohnung += GEWONNEN_PUNKTE
+                    self.quit = True
+                if learning_mode:
+                    self.quit = True
+            else:
+                self.quit = True
+                belohnung += VERLOREN_PUNKTE
+        if self.grafik:
+
+            self.surface.fill(color=(0, 0, 0))
+            pygame.draw.circle(
+                surface=self.surface,
+                color=(255, 255, 255),
+                center=(self.ball_x, self.ball_y),
+                radius=2,
+                width=0,
+            )
+            pygame.draw.line(
+                surface=self.surface,
+                color=(255, 0, 0),
+                start_pos=(BILDSCHIRM_X - 1, self.schlaeger_y),
+                end_pos=(BILDSCHIRM_X - 1, self.schlaeger_y + SCHLAEGER_LAENGE),
+            )
+            if evts := pygame.event.get():
+                for evt in evts:
+                    if evt.type == pygame.QUIT:
+                        quit = True
+            pygame.display.flip()
+            # sleep(0.1)
+            # if quit:
+            #     pygame.quit()
+        return (
+            np.array(
+                [
+                    self.schlaeger_y,
+                    self.ball_x,
+                    self.ball_y,
+                    self.ball_geschwindigkeit,
+                    self.ball_richtung,
+                ]
+            ),
+            belohnung,
+            self.quit,
+        )
+
+    def quit_grafik():
+        pygame.quit()
---
+++ Code/pong_neuronal.py
@@ -0,0 +1,172 @@
+import tensorflow as tf
+import keras
+import pong
+from keras import Sequential
+import numpy as np
+from typing import Callable
+import random
+import pandas as pd
+
+n_inputs = 5  # == env.observation_space.shape[0]
+
+model = Sequential(
+    [
+        keras.layers.Dense(20, activation="sigmoid", input_shape=[n_inputs]),
+        keras.layers.Dense(20, activation="relu"),
+        keras.layers.Dense(20, activation="relu"),
+        # keras.layers.Dense(10, activation="relu"),
+        # evtl mehr versteckte Schichten
+        keras.layers.Dense(
+            3,
+            activation="linear",  # bei mehr als einem Output Softmax-Aktivierungsfunktion
+        ),
+    ]
+)
+
+n_iterrations = 150
+n_episodes_per_update = 50
+n_max_steps = 200
+discount_factor = 0.95
+optimizer = keras.optimizers.Adam()
+loss_fn = keras.losses.binary_crossentropy
+history = []
+progress = 10
+
+
+def play_one_step(
+    game: pong.Game,
+    obs: np.ndarray,
+    model: keras.Model,
+    loss_fn: Callable,
+    zufaelligkeit: float,
+):
+    with tf.GradientTape() as tape:
+        probabilities = model(np.reshape(obs, (1, 5)))
+        if random.random() > zufaelligkeit:
+            action = np.argmax(probabilities.numpy()[0], axis=0)
+        else:
+            action = random.randint(0, 2)
+        y_target = tf.cast(
+            np.array([1 if i == action else 0 for i in range(3)], ndmin=2), tf.float32
+        )
+        loss = tf.reduce_mean(loss_fn(y_target, probabilities))
+    grads = tape.gradient(loss, model.trainable_variables)
+    obs, reward, done = game.pong_step(action)
+    return obs, reward, done, grads
+
+
+def play_multible_episodes(
+    game: pong.Game,
+    n_episodes: int,
+    n_max_steps: int,
+    model: keras.Model,
+    loss_fn: Callable,
+    zufaelligkeit: float,
+    progress: int,
+):
+    all_rewards = []
+    all_grads = []
+    winkel = int(5 + (progress) / 10 * 40)
+    for episode in range(n_episodes):
+        current_rewards = []
+        current_grads = []
+        obs = game.reset(winkel=winkel)
+        while True:
+            obs, reward, done, grads = play_one_step(
+                game,
+                obs,
+                model,
+                loss_fn,
+                zufaelligkeit,
+            )
+            current_rewards.append(reward)
+            current_grads.append(grads)
+            if done:
+                break
+        all_rewards.append(current_rewards)
+        all_grads.append(current_grads)
+
+        all_final_rewards = discount_rewards(current_rewards, discount_factor)
+        all_mean_grads = []
+        for var_index in range(len(model.trainable_variables)):
+            mean_grads = tf.reduce_mean(
+                [
+                    final_reward * current_grads[step][var_index]
+                    for step, final_reward in enumerate(all_final_rewards)
+                ],
+                axis=0,
+            )
+            all_mean_grads.append(mean_grads)
+        optimizer.apply_gradients(zip(all_mean_grads, model.trainable_variables))
+        print(
+            game.ball_y,
+            game.schlaeger_y,
+            zufaelligkeit,
+            reward,
+            sum(all_final_rewards),
+            progress,
+        )
+        history.append(
+            (
+                game.ball_y,
+                game.schlaeger_y,
+                zufaelligkeit,
+                reward,
+                sum(all_final_rewards),
+            )
+        )
+    return all_rewards, all_grads
+
+
+def discount_rewards(rewards: list, discount_factor: float):
+    discounted = np.array(rewards)
+    for step in range(len(rewards) - 2, -1, -1):
+        discounted[step] += discounted[step + 1] * discount_factor
+    return discounted
+
+
+def discount_and_normalize_rewards(all_rewards: list, discount_factor: float):
+    all_discounted_rewards = [
+        discount_rewards(rewards, discount_factor) for rewards in all_rewards
+    ]
+    flat_rewards = np.concatenate(all_discounted_rewards)
+    reward_mean = flat_rewards.mean()  # durchschnitt des reward arrays
+    reward_std = flat_rewards.std()  # standartabweichung des reward arrays
+    return [
+        (discount_rewards - reward_mean)
+        / reward_std  # herabgesetzte rewards - durchschnitt / standartabweichung
+        for discount_rewards in all_discounted_rewards  # für jeden reward im array aller rewards
+    ]
+
+
+if __name__ == "__main__":
+    game = pong.Game(mit_grafik=False)
+    for rounds in range(progress):
+        for interation in range(n_iterrations):
+            zufaelligkeit = 0.5 * (n_iterrations - interation) / n_iterrations
+            all_rewards, all_grads = play_multible_episodes(
+                game,
+                n_episodes_per_update,
+                n_max_steps,
+                model,
+                loss_fn,
+                zufaelligkeit,
+                rounds,
+            )
+            # all_final_rewards = discount_and_normalize_rewards(all_rewards, discount_factor)
+            # all_mean_grads = []
+            # for var_index in range(len(model.trainable_variables)):
+            #     mean_grads = tf.reduce_mean(
+            #         [
+            #             final_reward * all_grads[episode_index][step][var_index]
+            #             for episode_index, final_rewards in enumerate(all_final_rewards)
+            #             for step, final_reward in enumerate(final_rewards)
+            #         ],
+            #         axis=0,
+            #     )
+            #     all_mean_grads.append(mean_grads)
+            # optimizer.apply_gradients(zip(all_mean_grads, model.trainable_variables))
+        # pd.DataFrame.from_records(
+        #     history, columns=[("ball_y", "schlaeger_y", "zufaelligkeit", "reward")]
+        # ).to_csv("history.csv")
+    game.self.quit_grafik()
